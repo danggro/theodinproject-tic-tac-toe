d65243f1a51bd7a071c9d4d6707b03b69978406d
danggro <anangdwin55@gmail.com>
2023-11-08 23:13:02 +0700

    add player name

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ function GameController() {
   const allButton = document.getElementsByTagName('main')[0].children
   const winnerElement = document.getElementById('winner')
 
+  const formPlayer = document.getElementById('form-player')
+  const turnElement = document.getElementById('turn')
+
   let turn = true
   let totalTurn = 0
-
+  let player = []
   const logicWinArray = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,6 +34,18 @@ function GameController() {
     [2, 4, 6],
   ]
   const board = Gameboard()
+
+  function startGame() {
+    const player1Name = document.getElementById('player1').value
+    const player2Name = document.getElementById('player2').value
+    for (item of allButton) {
+      item.removeAttribute('disabled')
+    }
+    formPlayer.style.setProperty('display', 'none')
+    player = [player1Name, player2Name]
+
+    turnElement.innerHTML = `Player ${player[0]}'s turn`
+  }
 
   function action(event) {
     event.target.setAttribute('disabled', '')
@@ -51,15 +66,18 @@ function GameController() {
     } else if (totalTurn === 9) {
       revealWinnerElement()
       winnerElement.children[0].innerHTML = 'Draw'
+      turnElement.style.setProperty('display', 'none')
     }
   }
 
   function switchPlayerTurn(event) {
     if (turn) {
+      turnElement.innerHTML = `Player ${player[1]}'s turn`
       event.target.innerHTML = 'X'
       board.set([Number(event.target.id) - 1], 'X')
       turn = !turn
     } else {
+      turnElement.innerHTML = `Player ${player[0]}'s turn`
       event.target.innerHTML = 'O'
       board.set([Number(event.target.id) - 1], 'O')
       turn = !turn
@@ -69,10 +87,11 @@ function GameController() {
   function winHandler(logic) {
     if (logic === 'XXX' || logic === 'OOO') {
       revealWinnerElement()
+      turnElement.style.setProperty('display', 'none')
       if (logic === 'XXX') {
-        winnerElement.children[0].innerHTML = 'X Win'
+        winnerElement.children[0].innerHTML = `Player ${player[0]} win`
       } else {
-        winnerElement.children[0].innerHTML = 'O Win'
+        winnerElement.children[0].innerHTML = `Player ${player[1]} win`
       }
     }
   }
@@ -95,10 +114,12 @@ function GameController() {
     hideWinnerElement()
     turn = true
     totalTurn = 0
+    turnElement.innerHTML = `Player ${player[0]}'s turn`
+    turnElement.style.setProperty('display', 'inline')
     board.reset()
   }
 
-  return { reset, action }
+  return { reset, action, startGame }
 }
 
 const gameController = GameController()
